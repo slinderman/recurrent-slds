vbem and meanfield seem to be working with the rslds.  still need to fix the elbo calculation though

examples/bernoulli_lorenz.py
@@ -13,7 +13,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 import matplotlib.gridspec as gridspec
 from matplotlib.font_manager import FontProperties
-from mpl_toolkits.mplot3d import Axes3D
 
 import seaborn as sns
 color_names = ["windows blue",
@@ -42,7 +41,7 @@ from autoregressive.models import ARWeakLimitStickyHDPHMM
 
 from rslds.decision_list import DecisionList
 from pypolyagamma.distributions import MultinomialRegression, BernoulliRegression
-from rslds.rslds import RecurrentSLDS, RecurrentOnlySLDS, StickyRecurrentOnlySLDS
+from rslds.rslds import PGRecurrentSLDS, PGRecurrentOnlySLDS, StickyPGRecurrentOnlySLDS
 from rslds.util import logistic
 
 ### Global parameters
@@ -745,7 +744,7 @@ def fit_rslds(inputs, z_init, x_init, y, mask, dl_reg, C_init):
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
 
-    rslds = RecurrentSLDS(
+    rslds = PGRecurrentSLDS(
         D_in=D_latent,
         trans_params=dict(sigmasq_A=10000., sigmasq_b=10000.,
                           A=np.hstack((np.zeros((K - 1, K)), dl_reg.A)),
@@ -777,7 +776,7 @@ def fit_roslds(inputs, z_init, x_init, y, mask, dl_reg, C_init,
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
 
-    rslds = RecurrentOnlySLDS(
+    rslds = PGRecurrentOnlySLDS(
         trans_params=dict(sigmasq_A=10000., sigmasq_b=10000.,
                           A=np.hstack((np.zeros((K-1, K)), dl_reg.A)),
                           b=dl_reg.b),
@@ -809,7 +808,7 @@ def fit_sticky_inputonly_rslds(inputs, z_init, x_init, y, mask, dl_reg, C_init,
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
 
-    rslds = StickyRecurrentOnlySLDS(
+    rslds = StickyPGRecurrentOnlySLDS(
         D_in=D_latent,
         trans_params=dict(sigmasq_A=100., sigmasq_b=100., kappa=1.0,
                           A=np.hstack((np.zeros((K-1, K)), dl_reg.A)),

examples/nascar.py
@@ -38,8 +38,8 @@ from pypolyagamma.distributions import MultinomialRegression
 from pyslds.models import HMMSLDS
 
 from rslds.decision_list import DecisionList
-from rslds.rslds import RecurrentSLDS, StickyRecurrentSLDS, \
-    RecurrentOnlySLDS, StickyRecurrentOnlySLDS
+from rslds.rslds import PGRecurrentSLDS, StickyPGRecurrentSLDS, \
+    PGRecurrentOnlySLDS, StickyPGRecurrentOnlySLDS
 from rslds.util import compute_psi_cmoments
 
 ### Global parameters
@@ -502,7 +502,7 @@ def simulate_nascar():
                            A=C, sigmasq=1e-5 *np.ones(D_obs),
                            alpha_0=2.0, beta_0=2.0)
 
-    model = RecurrentSLDS(
+    model = PGRecurrentSLDS(
         trans_params=dict(A=np.hstack((np.zeros((K_true-1, K_true)), reg_W)), b=reg_b,
                           sigmasq_A=100., sigmasq_b=100.),
         init_state_distn='uniform',
@@ -694,7 +694,7 @@ def fit_rslds(inputs, z_init, x_init, y, mask, dl_reg, C_init,
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
 
-    rslds = RecurrentSLDS(
+    rslds = PGRecurrentSLDS(
         trans_params=dict(sigmasq_A=10000., sigmasq_b=10000.,
                           A=np.hstack((np.zeros((K - 1, K)), dl_reg.A)),
                           b=dl_reg.b),
@@ -741,7 +741,7 @@ def fit_sticky_rslds(inputs, z_init, x_init, y, mask, dl_reg, C_init,
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
 
-    rslds = StickyRecurrentSLDS(
+    rslds = StickyPGRecurrentSLDS(
         D_in=D_latent,
         trans_params=dict(sigmasq_A=10000., sigmasq_b=10000., kappa=100.,
                           A=np.hstack((np.zeros((K - 1, K)), dl_reg.A)),
@@ -788,7 +788,7 @@ def fit_roslds(inputs, z_init, x_init, y, mask, dl_reg, C_init,
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
 
-    rslds = RecurrentOnlySLDS(
+    rslds = PGRecurrentOnlySLDS(
         trans_params=dict(sigmasq_A=10000., sigmasq_b=10000.,
                           A=np.hstack((np.zeros((K-1, K)), dl_reg.A)),
                           b=dl_reg.b),
@@ -834,7 +834,7 @@ def fit_sticky_roslds(inputs, z_init, x_init, y, mask, dl_reg, C_init,
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
 
-    rslds = StickyRecurrentOnlySLDS(
+    rslds = StickyPGRecurrentOnlySLDS(
         trans_params=dict(sigmasq_A=10000., sigmasq_b=10000.,
                           kappa=1., sigmasq_kappa=1.0,
                           A=np.hstack((np.zeros((K-1, K)), dl_reg.A)),

examples/nonconj_inhmm.py
@@ -16,7 +16,7 @@ from hips.plotting.colormaps import gradient_cmap
 cmap = gradient_cmap(colors)
 
 from pybasicbayes.distributions import Gaussian
-from rslds.inhmm import InputHMM, SoftmaxInputHMM, SoftmaxInputOnlyHMM
+from rslds.models import InputHMM, SoftmaxInputHMM, SoftmaxInputOnlyHMM
 
 
 #################################################
@@ -55,7 +55,7 @@ dataset = [true_model.generate(T, covariates=covariate_seq[:,:D_in]) for _ in ra
 # Generate inference test model - initialized randomly #
 ########################################################
 test_model = \
-    SoftmaxInputOnlyHMM(obs_distns=[Gaussian(**obs_hypparams) for state in range(Nmax)],
+    SoftmaxInputHMM(obs_distns=[Gaussian(**obs_hypparams) for state in range(Nmax)],
                     init_state_concentration=1.0,
                     D_in=D_in,)
 
@@ -65,13 +65,15 @@ for (obs, covs), _ in dataset:
 # run sampler
 print("io hmm sampling")
 num_iter = 200
-A_smpls = np.zeros((num_iter,) + test_model.trans_distn.W.shape)
+W_smpls = np.zeros((num_iter,) + test_model.trans_distn.W.shape)
 z_smpls = np.zeros((num_iter, T))
+import ipdb; ipdb.set_trace()
 for itr in range(num_iter):
-    test_model.resample_model(num_procs=0)
+    # test_model.resample_model()
+    test_model.EM_step()
     if itr % 20 == 0:
         print("Iter {}.  LL:  {:.1f}".format(itr, test_model.log_likelihood()))
-    A_smpls[itr, :, :] = test_model.trans_distn.W
+    W_smpls[itr, :, :] = test_model.trans_distn.W
     z_smpls[itr] = test_model.stateseqs[0]
 
 # Get the inferred state sequence

examples/nonconj_nascar.py
@@ -3,7 +3,7 @@ import pickle
 
 import numpy as np
 import numpy.random as npr
-npr.seed(1)
+npr.seed(1234)
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
@@ -35,8 +35,7 @@ from pyslds.util import get_empirical_ar_params
 from pylds.util import random_rotation
 
 from pyslds.models import HMMSLDS
-from rslds.rslds import RecurrentSLDS
-from rslds.nonconj_rslds import SoftmaxRecurrentOnlySLDS
+from rslds.models import SoftmaxRecurrentOnlySLDS, SoftmaxRecurrentSLDS
 
 ### Global parameters
 T, K, K_true, D_obs, D_latent = 10000, 4, 4, 10, 2
@@ -44,7 +43,7 @@ mask_start, mask_stop = 0, 0
 N_iters = 1000
 
 # Save / cache the outputs
-CACHE_RESULTS = False
+CACHE_RESULTS = True
 RUN_NUMBER = 1
 RESULTS_DIR = os.path.join("results", "nascar", "run{:03d}".format(RUN_NUMBER))
 
@@ -262,10 +261,7 @@ def plot_trans_probs(reg,
     XX,YY = np.meshgrid(np.linspace(*xlim,n_pts),
                         np.linspace(*ylim,n_pts))
     XY = np.column_stack((np.ravel(XX), np.ravel(YY)))
-
-    D_reg = reg.D_in
-    inputs = np.hstack((np.zeros((n_pts**2, D_reg-2)), XY))
-    test_prs = reg.pi(inputs)
+    test_prs = reg.get_trans_matrices(XY)[:,0,:]
 
     if ax is None:
         fig = plt.figure(figsize=(10,6))
@@ -606,7 +602,7 @@ def make_rslds_parameters(C_init):
         Regression(
             A=As[k],
             sigma=np.eye(D_latent),
-            nu_0=D_latent + 2,
+            nu_0=D_latent + 1000,
             S_0=np.eye(D_latent),
             M_0=np.hstack((np.eye(D_latent), np.zeros((D_latent, 1)))),
             K_0=np.eye(D_latent + 1),
@@ -628,7 +624,7 @@ def make_rslds_parameters(C_init):
 
 @cached("slds")
 def fit_slds(inputs, z_init, x_init, y, mask, C_init,
-              N_iters=10000):
+              N_iters=1000):
     print("Fitting standard SLDS")
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
@@ -665,7 +661,7 @@ def fit_slds(inputs, z_init, x_init, y, mask, C_init,
 
     return slds, lps, z_smpls, x_test
 
-@cached("rslds")
+# @cached("rslds")
 def fit_rslds(inputs, z_init, x_init, y, mask, C_init,
               true_model=None, initialization="none", N_iters=10000):
     print("Fitting rSLDS")
@@ -716,8 +712,12 @@ def fit_rslds(inputs, z_init, x_init, y, mask, C_init,
     return rslds, lps, z_smpls, x_test
 
 # @cached("rslds_variational")
-def fit_rslds_variational(inputs, z_init, x_init, y, mask, C_init,
-              true_model=None, initialization="none",  N_gibbs=100, N_iters=10000):
+def fit_rslds_variational(
+        inputs, y, mask,
+        x_init=None, z_init=None, C_init=None, initialization="none",
+        true_model=None,
+        N_iters=10000):
+
     print("Fitting rSLDS")
     init_dynamics_distns, dynamics_distns, emission_distns = \
         make_rslds_parameters(C_init)
@@ -754,15 +754,11 @@ def fit_rslds_variational(inputs, z_init, x_init, y, mask, C_init,
     elif initialization == "given":
         print("Initializing with given states")
         rslds.emission_distns[0].J_0 = 1e2 * np.eye(D_latent + 1)
-        rslds.states_list[0].stateseq = z_init.copy()
+        rslds.states_list[0].stateseq = z_init.astype(np.int32).copy()
         rslds.states_list[0].gaussian_states = x_init.copy()
-        for _ in progprint_xrange(N_gibbs):
-            rslds.resample_model()
 
-    elif initialization == "gibbs":
-        print("Initializing with Gibbs sampling")
-        for _ in progprint_xrange(N_gibbs):
-            rslds.resample_model()
+    else:
+        print("no initialization")
 
     rslds._init_mf_from_gibbs()
 
@@ -771,12 +767,11 @@ def fit_rslds_variational(inputs, z_init, x_init, y, mask, C_init,
     z_smpls = [np.argmax(rslds.states_list[0].expected_states, axis=1)]
     for _ in progprint_xrange(N_iters):
         vlbs.append(rslds.meanfield_coordinate_descent_step(compute_vlb=True))
-        z_smpls.append(np.argmax(rslds.states_list[0].expected_states, axis=1))
+        z_smpls.append(rslds.states_list[0].stateseq.copy())
 
-    x_smpl = rslds.states_list[0].smoothed_mus
+    x_smpl = rslds.states_list[0].smoothed_mus.copy()
     z_smpls = np.array(z_smpls)
     vlbs = np.array(vlbs)
-
 
     if true_model is not None:
         print("True logpi:\n{}".format(true_model.trans_distn.logpi))
@@ -788,29 +783,93 @@ def fit_rslds_variational(inputs, z_init, x_init, y, mask, C_init,
     return rslds, vlbs, z_smpls, x_smpl
 
 
+
+# @cached("rslds_vbem")
+def fit_rslds_vbem(
+        inputs, y, mask,
+        x_init=None, z_init=None, C_init=None, initialization="none",
+        true_model=None,
+        N_iters=10000):
+
+    print("Fitting rSLDS")
+    init_dynamics_distns, dynamics_distns, emission_distns = \
+        make_rslds_parameters(C_init)
+
+    rslds = SoftmaxRecurrentOnlySLDS(
+    # rslds = SoftmaxRecurrentSLDS(
+        init_state_distn='uniform',
+        init_dynamics_distns=init_dynamics_distns,
+        dynamics_distns=dynamics_distns,
+        emission_distns=emission_distns,
+        fixed_emission=False,
+        alpha=3.)
+
+    # Set the prior precision for the dynamics params
+    rslds.add_data(y, inputs=inputs, mask=mask)
+
+    if initialization == "true":
+        print("Initializing with true model")
+        rslds.emission_distns[0].J_0 = 1e2 * np.eye(D_latent + 1)
+        rslds.trans_distn.W = true_model.trans_distn.W.copy()
+        rslds.trans_distn.b = true_model.trans_distn.b.copy()
+
+        for rdd, tdd in zip(rslds.dynamics_distns, true_model.dynamics_distns):
+            rdd.A = tdd.A.copy()
+            rdd.sigma = tdd.sigma.copy()
+
+        rslds.emission_distns[0].A = true_model.emission_distns[0].A.copy()
+        rslds.emission_distns[0].sigmasq_flat = true_model.emission_distns[0].sigmasq_flat.copy()
+        rslds.emission_distns[0].J_0 = 1e2 * np.eye(D_latent+1)
+
+        rslds.states_list[0].stateseq = true_model.states_list[0].stateseq.copy()
+        rslds.states_list[0].gaussian_states = true_model.states_list[0].gaussian_states.copy()
+
+        rslds._init_mf_from_gibbs()
+        rslds._vb_E_step()
+
+    elif initialization == "given":
+        print("Initializing with given states")
+        rslds.emission_distns[0].J_0 = 1e2 * np.eye(D_latent + 1)
+        rslds.states_list[0].stateseq = z_init.astype(np.int32).copy()
+        rslds.states_list[0].gaussian_states = x_init.copy()
+        rslds._init_mf_from_gibbs()
+        rslds._vb_M_step()
+        rslds._vb_E_step()
+
+    else:
+        print("no initialization")
+        rslds._init_mf_from_gibbs()
+
+    # Fit the model
+    vlbs = []
+    z_smpls = [rslds.states_list[0].stateseq.copy()]
+    for _ in progprint_xrange(N_iters):
+        rslds.VBEM_step()
+        vlbs.append(rslds.VBEM_ELBO())
+        z_smpls.append(rslds.states_list[0].stateseq.copy())
+
+    x_smpl = rslds.states_list[0].smoothed_mus.copy()
+    z_smpls = np.array(z_smpls)
+    vlbs = np.array(vlbs)
+
+    if true_model is not None:
+        print("True logpi:\n{}".format(true_model.trans_distn.logpi))
+        print("True W:\n{}".format(true_model.trans_distn.W))
+
+    print("Inf logpi:\n{}".format(rslds.trans_distn.logpi))
+    print("Inf W:\n{}".format(rslds.trans_distn.W))
+
+    return rslds, vlbs, z_smpls, x_smpl
+
 if __name__ == "__main__":
     ## Simulate NASCAR data
     true_model, inputs, z_true, x_true, y, mask = simulate_nascar()
 
-    # plot_most_likely_dynamics(true_model.trans_distn,
-    #                           true_model.dynamics_distns,
-    #                           figsize=(3,1.5))
-
-    # plot_all_dynamics(true_model.dynamics_distns)
-
     ## Run PCA to get 2D dynamics
-    # x_init, C_init = fit_factor_analysis(y, mask=mask)
     x_init, C_init = fit_pca(y)
 
     ## Fit an ARHMM for initialization
     arhmm, z_init = fit_arhmm(x_init)
-    # plot_trajectory_and_probs(
-    #     z_init[1:], x_init[1:],
-    #     title="Sticky ARHMM")
-    # plt.show()
-
-    # plot_all_dynamics(arhmm.obs_distns,
-    #                   filename="sticky_arhmm_dynamics.png")
 
     ## Fit a standard SLDS
     slds, slds_lps, slds_z_smpls, slds_x = \
@@ -818,24 +877,38 @@ if __name__ == "__main__":
 
     ## Fit a recurrent SLDS
     # rslds, rslds_lps, rslds_z_smpls, rslds_x = \
-    #     fit_rslds(inputs, z_init, x_init, y, mask, C_init,
-    #               true_model=true_model, N_iters=1000)
+    #     fit_rslds_variational(inputs, y, mask,
+    #                           z_init=z_init, x_init=x_init, C_init=C_init,
+    #                           initialization="given",
+    #                           true_model=true_model, N_iters=100)
+
+    # rslds, rslds_lps, rslds_z_smpls, rslds_x = \
+    #     fit_rslds_vbem(inputs, z_init, x_init, y, mask, C_init=C_init,
+    #                    true_model=true_model, N_iters=100,
+    #                    initialization="true")
 
     rslds, rslds_lps, rslds_z_smpls, rslds_x = \
-        fit_rslds_variational(inputs, z_init, x_init, y, mask, C_init=C_init,
-                  true_model=true_model, N_iters=100, initialization="given")
+        fit_rslds_vbem(inputs, y, mask,
+                       z_init=z_init, x_init=x_init, C_init=C_init,
+                       initialization="given",
+                       true_model=true_model, N_iters=100)
 
-    # plot_trajectory_and_probs(
-    #     rslds_z_smpls[-1][1:], rslds_x[1:],
-    #     trans_distn=rslds.trans_distn,
-    #     title="Recurrent SLDS",
-    #     filename="rslds.png")
-    #
-    # plot_all_dynamics(rslds.dynamics_distns)
-    #
-    # plot_z_samples(rslds_z_smpls,
-    #                plt_slice=(0,1000),
-    #                filename="rslds_zsamples.png")
+    # rslds, rslds_lps, rslds_z_smpls, rslds_x = \
+    #     fit_rslds_vbem(inputs, y, mask,
+    #                    initialization="none",
+    #                    N_iters=100)
+
+    plot_trajectory_and_probs(
+        rslds_z_smpls[-1][1:], rslds_x[1:],
+        trans_distn=rslds.trans_distn,
+        title="Recurrent SLDS",
+        filename="rslds.png")
+
+    plot_all_dynamics(rslds.dynamics_distns)
+
+    plot_z_samples(rslds_z_smpls,
+                   plt_slice=(0,1000),
+                   filename="rslds_zsamples.png")
 
     ## Generate from the model
     T_gen = 2000

rslds/models.py
@@ -1,49 +1,17 @@
 import numpy as np
-from pybasicbayes.util.stats import sample_discrete
-from pyhsmm.internals import initial_state
-from pyhsmm.internals.hmm_states import HMMStatesEigen
-from pyhsmm.models import _HMMGibbsSampling, _HMMMeanField
-from rslds.transitions import InputHMMTransitions, InputOnlyHMMTransitions, \
-    StickyInputOnlyHMMTransitions, SoftmaxInputHMMTransitions, SoftmaxInputOnlyHMMTransitions
 
+from pyhsmm.models import _HMMGibbsSampling, _HMMEM, _HMMMeanField
+from pyhsmm.internals.initial_state import UniformInitialState
 
-##############################################################################
-# Outline for extending an HMM to IO-HMM with mix-ins                        #
-#    1. create a transitions base class that creates stacks of transition    #
-#       matrices given a matrix of inputs (see transitions.py)               #
-#    2. create a Gibbs mix-in that resamples transition parameters given     #
-#       realized discrete state sequences                                    #
-#                                                                            #
-##############################################################################
+from autoregressive.models import _ARMixin
+from autoregressive.util import AR_striding
 
+from pyslds.models import _SLDSGibbsMixin, _SLDSVBEMMixin, _SLDSMeanFieldMixin
 
-class InputHMMStates(HMMStatesEigen):
+from rslds.states import InputHMMStates, PGRecurrentSLDSStates, SoftmaxRecurrentSLDSStates
+import rslds.transitions as transitions
 
-    def __init__(self, covariates, *args, **kwargs):
-        self.covariates = covariates
-        super(InputHMMStates, self).__init__(*args, **kwargs)
-
-    @property
-    def trans_matrix(self):
-        return self.model.trans_distn.get_trans_matrices(self.covariates)
-
-    @staticmethod
-    def _sample_markov_hetero(T, trans_matrix_seq, init_state_distn):
-        """utility function - just like sample_markov but with a stack of trans mats"""
-        out = np.empty(T, dtype=np.int32)
-        out[0] = sample_discrete(init_state_distn.ravel())
-        for t in range(1, T):
-            out[t] = sample_discrete(trans_matrix_seq[t - 1, out[t - 1], :].ravel())
-        return out
-
-    def generate_states(self):
-        self.stateseq = InputHMMStates._sample_markov_hetero(
-            T=self.T,
-            trans_matrix_seq=self.trans_matrix,  # stack of matrices
-            init_state_distn=np.ones(self.num_states) / float(self.num_states))
-
-
-### HMM Model with time varying covariates
+### Input-driven HMMs
 class _InputHMMMixin(object):
 
     # Subclasses must specify the type of transition model
@@ -71,7 +39,7 @@ class _InputHMMMixin(object):
 
         if init_state_distn is not None:
             if init_state_distn == 'uniform':
-                self.init_state_distn = initial_state.UniformInitialState(model=self)
+                self.init_state_distn = UniformInitialState(model=self)
             else:
                 self.init_state_distn = init_state_distn
         else:
@@ -119,35 +87,45 @@ class _InputHMMMixin(object):
         )
         self._clear_caches()
 
+    ## EM
+    def _M_step_trans_distn(self):
+        self.trans_distn.max_likelihood(
+            expected_stateseqs=[s.expected_states for s in self.states_list],
+            expected_covseqs=[s.covariates for s in self.states_list]
+        )
+
     ### Mean Field
     def meanfield_update_trans_distn(self):
         # self.trans_distn.meanfieldupdate(
         #     [s.expected_transcounts for s in self.states_list])
         # self._clear_caches()
-        # TODO: Update to take in sufficient statistics of x and z
-        pass
+        raise NotImplementedError
 
-class InputHMM(_InputHMMMixin, _HMMGibbsSampling, _HMMMeanField):
-    _trans_class = InputHMMTransitions
+
+class InputHMM(_InputHMMMixin, _HMMGibbsSampling, _HMMEM, _HMMMeanField):
+    _trans_class = transitions.InputHMMTransitions
     _states_class = InputHMMStates
 
+
 class InputOnlyHMM(InputHMM):
-    _trans_class = InputOnlyHMMTransitions
+    _trans_class = transitions.InputOnlyHMMTransitions
+
 
 class StickyInputOnlyHMM(InputHMM):
-    _trans_class = StickyInputOnlyHMMTransitions
+    _trans_class = transitions.StickyInputOnlyHMMTransitions
+
 
 class SoftmaxInputHMM(InputHMM):
-    _trans_class = SoftmaxInputHMMTransitions
+    _trans_class = transitions.SoftmaxInputHMMTransitions
+
 
 class SoftmaxInputOnlyHMM(InputHMM):
-    _trans_class = SoftmaxInputOnlyHMMTransitions
+    _trans_class = transitions.SoftmaxInputOnlyHMMTransitions
+
 
 ### ARHMM's
-from autoregressive.models import _ARMixin
-from autoregressive.util import AR_striding
 class InputARHMM(_InputHMMMixin, _ARMixin, _HMMGibbsSampling):
-    _trans_class = InputHMMTransitions
+    _trans_class = transitions.InputHMMTransitions
     _states_class = InputHMMStates
 
     def add_data(self, data, covariates=None, strided=False, **kwargs):
@@ -159,17 +137,22 @@ class InputARHMM(_InputHMMMixin, _ARMixin, _HMMGibbsSampling):
         assert strided_data.shape[0] == lagged_covariates.shape[0]
         super(InputARHMM, self).add_data(data=strided_data,covariates=lagged_covariates,**kwargs)
 
+
 class InputOnlyARHMM(InputARHMM):
-    _trans_class = InputOnlyHMMTransitions
+    _trans_class = transitions.InputOnlyHMMTransitions
+
 
 class StickyInputOnlyARHMM(InputARHMM):
-    _trans_class = StickyInputOnlyHMMTransitions
+    _trans_class = transitions.StickyInputOnlyHMMTransitions
+
 
 class SoftmaxInputARHMM(InputARHMM):
-    _trans_class = SoftmaxInputHMMTransitions
+    _trans_class = transitions.SoftmaxInputHMMTransitions
+
 
 class SoftmaxInputOnlyARHMM(InputARHMM):
-    _trans_class = SoftmaxInputOnlyHMMTransitions
+    _trans_class = transitions.SoftmaxInputOnlyHMMTransitions
+
 
 ### Recurrent ARHMM's
 class RecurrentARHMM(InputARHMM):
@@ -182,6 +165,7 @@ class RecurrentARHMM(InputARHMM):
         covariates = np.row_stack((np.zeros(self.D),
                                    data[:-1]))
         super(RecurrentARHMM, self).add_data(data, covariates=covariates, **kwargs)
+
 
     def generate(self, T=100, keep=True, init_data=None, covariates=None, with_noise=True):
         from pybasicbayes.util.stats import sample_discrete
@@ -221,13 +205,115 @@ class RecurrentARHMM(InputARHMM):
 
 
 class RecurrentOnlyARHMM(RecurrentARHMM):
-    _trans_class = InputOnlyHMMTransitions
+    _trans_class = transitions.InputOnlyHMMTransitions
+
 
 class StickyRecurrentOnlyARHMM(RecurrentARHMM):
-    _trans_class = StickyInputOnlyHMMTransitions
+    _trans_class = transitions.StickyInputOnlyHMMTransitions
+
 
 class SoftmaxRecurrentARHMM(RecurrentARHMM):
-    _trans_class = SoftmaxInputHMMTransitions
+    _trans_class = transitions.SoftmaxInputHMMTransitions
+
 
 class SoftmaxRecurrentOnlyARHMM(RecurrentARHMM):
-    _trans_class = SoftmaxInputOnlyHMMTransitions
+    _trans_class = transitions.SoftmaxInputOnlyHMMTransitions
+
+
+### Stick-breaking transition models with Pólya-gamma augmentation
+class _RecurrentSLDSBase(object):
+    def __init__(self, dynamics_distns, emission_distns, init_dynamics_distns,
+                 fixed_emission=False, **kwargs):
+
+        self.fixed_emission = fixed_emission
+
+        super(_RecurrentSLDSBase, self).__init__(
+            dynamics_distns, emission_distns, init_dynamics_distns,
+            D_in=dynamics_distns[0].D_out, **kwargs)
+
+    def add_data(self, data, **kwargs):
+        self.states_list.append(
+                self._states_class(model=self, data=data, **kwargs))
+
+    def generate(self, T=100, keep=True, with_noise=True, **kwargs):
+        s = self._states_class(model=self, T=T, initialize_from_prior=True, **kwargs)
+        s.generate_states(with_noise=with_noise)
+        data = self._generate_obs(s, with_noise=with_noise)
+        if keep:
+            self.states_list.append(s)
+        return data, s.stateseq
+
+    def _generate_obs(self, s, with_noise=True):
+        if s.data is None:
+            s.data = s.generate_obs()
+        else:
+            # TODO: Handle missing data
+            raise NotImplementedError
+
+        return s.data, s.gaussian_states
+
+
+class PGRecurrentSLDS(_RecurrentSLDSBase, _SLDSGibbsMixin, InputHMM):
+
+    _states_class = PGRecurrentSLDSStates
+    _trans_class = transitions.InputHMMTransitions
+
+    def resample_trans_distn(self):
+        # Include the auxiliary variables used for state resampling
+        self.trans_distn.resample(
+            stateseqs=[s.stateseq for s in self.states_list],
+            covseqs=[s.covariates for s in self.states_list],
+            omegas=[s.trans_omegas for s in self.states_list]
+        )
+        self._clear_caches()
+
+    def resample_emission_distns(self):
+        if self.fixed_emission:
+            return
+        super(PGRecurrentSLDS, self).resample_emission_distns()
+
+
+class StickyPGRecurrentSLDS(PGRecurrentSLDS):
+    _trans_class = transitions.StickyInputHMMTransitions
+
+
+class PGRecurrentOnlySLDS(PGRecurrentSLDS):
+    _trans_class = transitions.InputOnlyHMMTransitions
+
+
+class StickyPGRecurrentOnlySLDS(PGRecurrentSLDS):
+    _trans_class = transitions.StickyInputOnlyHMMTransitions
+
+
+### Softmax transition models with variational inference
+class SoftmaxRecurrentSLDS(_RecurrentSLDSBase, _SLDSMeanFieldMixin, _SLDSVBEMMixin, InputHMM):
+    _states_class = SoftmaxRecurrentSLDSStates
+    _trans_class = transitions.SoftmaxInputHMMTransitions
+
+    def _M_step_trans_distn(self):
+        # self.trans_distn.max_likelihood(
+        #         expected_stateseqs=[s.expected_states for s in self.states_list],
+        #         expected_covseqs=[s.smoothed_mus[:-1] for s in self.states_list])
+        sum_tuples = lambda lst: list(map(sum, zip(*lst)))
+        self.trans_distn.max_likelihood(
+            stats=sum_tuples([s.E_trans_stats for s in self.states_list]))
+
+    def meanfield_update_trans_distn(self):
+        # Include the auxiliar variables of the lower bound
+        sum_tuples = lambda lst: list(map(sum, zip(*lst)))
+        self.trans_distn.meanfieldupdate(
+            stats=sum_tuples([s.E_trans_stats for s in self.states_list]))
+
+    def _init_mf_from_gibbs(self):
+        self.trans_distn._initialize_mean_field()
+        super(SoftmaxRecurrentSLDS, self)._init_mf_from_gibbs()
+
+    def meanfield_update_parameters(self):
+        self.meanfield_update_init_dynamics_distns()
+        self.meanfield_update_dynamics_distns()
+        self.meanfield_update_emission_distns()
+        super(SoftmaxRecurrentSLDS, self).meanfield_update_parameters()
+
+
+class SoftmaxRecurrentOnlySLDS(SoftmaxRecurrentSLDS):
+    _trans_class = transitions.SoftmaxInputOnlyHMMTransitions

rslds/transitions.py
@@ -159,7 +159,7 @@ import autograd.numpy as anp
 import autograd.scipy.misc as amisc
 from autograd import grad
 
-class SoftmaxInputHMMTransitions(object):
+class _SoftmaxInputHMMTransitionsBase(object):
     """
     Like above but with a softmax transition model.
 
@@ -192,26 +192,12 @@ class SoftmaxInputHMMTransitions(object):
         else:
             self.W = np.zeros((covariate_dim, num_states))
 
-        # Assume diagonal prior on vec(W)
-        # self.mu_W = mu_W * anp.ones((covariate_dim, num_states))
-        # self.sigmasq_W = sigmasq_W * anp.ones((covariate_dim, num_states))
-
         mu_0 = np.zeros(self.D_in) if mu_0 is None else mu_0
         Sigma_0 = np.eye(self.D_in) if Sigma_0 is None else Sigma_0
         assert mu_0.shape == (self.D_in,)
         assert Sigma_0.shape == (self.D_in, self.D_in)
         self.h_0 = np.linalg.solve(Sigma_0, mu_0)
         self.J_0 = np.linalg.inv(Sigma_0)
-
-        # HMC params
-        self.step_sz = 0.01
-        self.accept_rate = 0.9
-        self.target_accept_rate = 0.9
-
-        # Mean field natural parameters
-        self.mf_J = np.array([self.J_0.copy() for _ in range(self.D_out)])
-        self.mf_h = np.array([self.h_0.copy() for Jd in self.mf_J])
-        self._mf_Sigma = self._mf_mu = self._mf_mumuT = None
 
     ### HMC
     def get_log_trans_matrices(self, X):
@@ -242,6 +228,21 @@ class SoftmaxInputHMMTransitions(object):
         """
         log_trans_matrices = self.get_log_trans_matrices(X)
         return np.exp(log_trans_matrices)
+
+
+class _SoftmaxInputHMMTransitionsHMC(_SoftmaxInputHMMTransitionsBase):
+    def __init__(self, num_states, covariate_dim,
+                 mu_0=None, Sigma_0=None,
+                 logpi=None, W=None):
+
+        super(_SoftmaxInputHMMTransitionsHMC, self).__init__(
+            num_states, covariate_dim,
+            mu_0=mu_0, Sigma_0=Sigma_0, logpi=logpi, W=W)
+
+        # HMC params
+        self.step_sz = 0.01
+        self.accept_rate = 0.9
+        self.target_accept_rate = 0.9
 
     def joint_log_probability(self, logpi, W, stateseqs, covseqs):
         K, D = self.num_states, self.covariate_dim
@@ -288,6 +289,84 @@ class SoftmaxInputHMMTransitions(object):
 
         self.logpi = xf[:K**2].reshape((K, K))
         self.W = xf[K**2:].reshape((D, K))
+
+
+class _SoftmaxInputHMMTransitionsEM(_SoftmaxInputHMMTransitionsBase):
+
+    # def expected_log_probability(self, logpi, W, E_zs, E_xs):
+    #     K, D = self.num_states, self.covariate_dim
+    #
+    #     # todo: log prior
+    #
+    #     # Compute the objective
+    #     ll = 0
+    #     for z, x in zip(E_zs, E_xs):
+    #         T = z.shape[0]
+    #         assert z.shape == (T, K)
+    #         assert x.shape == (T - 1, D)
+    #
+    #         # Numerator
+    #         tmp = anp.dot(z[:-1], logpi) + anp.dot(x, W)
+    #         ll += anp.sum(tmp * z[1:])
+    #
+    #         # Denominator
+    #         Z = amisc.logsumexp(tmp, axis=1)
+    #         ll -= anp.sum(Z)
+    #     return ll
+    #
+    # def max_likelihood(self, expected_stateseqs=None, expected_covseqs=None):
+    #     K, D = self.num_states, self.covariate_dim
+    #
+    #     # Optimize with BFGS
+    #     def _objective(params):
+    #         K, D = self.num_states, self.covariate_dim
+    #         logpi = params[:K ** 2].reshape((K, K))
+    #         W = params[K ** 2:].reshape((D, K))
+    #         return -1 * self.expected_log_probability(
+    #             logpi, W, expected_stateseqs, expected_covseqs)
+    #
+    #     _grad_objective = grad(_objective)
+    #     x0 = np.concatenate((np.ravel(self.logpi), np.ravel(self.W)))
+    #
+    #     from scipy.optimize import minimize
+    #     res = minimize(_objective, x0, jac=_grad_objective, method="BFGS")
+    #     self.logpi = res.x[:K**2].reshape((K, K))
+    #     self.W = res.x[K**2:].reshape((D, K))
+    def max_likelihood(self, stats):
+        """
+        Update the expected transition matrix with a bunch of stats
+        :param stats: E_zp1_uT, E_uuT, E_u, a, lambda_bs from the states model
+        :param prob: minibatch probability
+        :param stepsize: svi step size
+        """
+        K, D = self.num_states, self.covariate_dim
+        E_u_zp1T, E_uuT, E_u, a, lambda_bs = stats
+
+        # Update statistics each row of A
+        for k in range(self.D_out):
+            # Jk = self.J_0 + 2 * lambda_bs[:,k][:,None,None] * E_uuT
+            Jk = self.J_0 + 2 * np.einsum('t, tij -> ij', lambda_bs[:, k], E_uuT)
+            hk = self.h_0 + E_u_zp1T[:, :, k].sum(0)
+            hk -= np.einsum('t, ti -> i', (0.5 - 2 * lambda_bs[:, k] * a), E_u)
+
+            # Update the mean field natural parameters
+            ak = np.linalg.solve(Jk, hk)
+            self.logpi[:,k] = ak[:K]
+            self.W[:,k] = ak[K:]
+
+
+class _SoftmaxInputHMMTransitionsMeanField(_SoftmaxInputHMMTransitionsBase):
+    def __init__(self, num_states, covariate_dim,
+                 mu_0=None, Sigma_0=None,
+                 logpi=None, W=None):
+        super(_SoftmaxInputHMMTransitionsMeanField, self).__init__(
+            num_states, covariate_dim,
+            mu_0=mu_0, Sigma_0=Sigma_0, logpi=logpi, W=W)
+
+        # Mean field natural parameters
+        self.mf_J = np.array([self.J_0.copy() for _ in range(self.D_out)])
+        self.mf_h = np.array([self.h_0.copy() for Jd in self.mf_J])
+        self._mf_Sigma = self._mf_mu = self._mf_mumuT = None
 
     ### Mean field
     @property
@@ -367,6 +446,12 @@ class SoftmaxInputHMMTransitions(object):
         self._set_standard_expectations()
 
 
+class SoftmaxInputHMMTransitions(_SoftmaxInputHMMTransitionsHMC,
+                                 _SoftmaxInputHMMTransitionsEM,
+                                 _SoftmaxInputHMMTransitionsMeanField):
+    pass
+
+
 class SoftmaxInputOnlyHMMTransitions(SoftmaxInputHMMTransitions):
     """
     Like above but with logpi constant for all rows (prev states)
@@ -436,6 +521,67 @@ class SoftmaxInputOnlyHMMTransitions(SoftmaxInputHMMTransitions):
 
         self.b = xf[:K]
         self.W = xf[K:].reshape((D, K))
+
+    ### EM
+    # def max_likelihood(self, expected_stateseqs=None, expected_covseqs=None):
+    #     K, D = self.num_states, self.covariate_dim
+    #
+    #     # Optimize with BFGS
+    #     def _objective(params):
+    #         K, D = self.num_states, self.covariate_dim
+    #         b = params[:K]
+    #         logpi = anp.tile(b[None, :], (K, 1))
+    #         W = params[K:].reshape((D, K))
+    #         return -1 * self.expected_log_probability(
+    #             logpi, W, expected_stateseqs, expected_covseqs)
+    #
+    #     _grad_objective = grad(_objective)
+    #     x0 = np.concatenate((np.ravel(self.b), np.ravel(self.W)))
+    #
+    #     from scipy.optimize import minimize
+    #     res = minimize(_objective, x0, jac=_grad_objective, method="BFGS")
+    #     self.b = res.x[:K].reshape((K,))
+    #     self.W = res.x[K:].reshape((D, K))
+    def max_likelihood(self, stats):
+        """
+        Update the expected transition matrix with a bunch of stats
+        :param stats: E_zp1_uT, E_uuT, E_u, a, lambda_bs from the states model
+        :param prob: minibatch probability
+        :param stepsize: svi step size
+        """
+        K, D = self.num_states, self.covariate_dim
+        E_u_zp1T, E_uuT, E_u, a, lambda_bs = stats
+        T = E_u.shape[0]
+
+        # Remember u = [z, x]
+        # Combine statistics across all preceding states, z,
+        # to get statistics of shape (1+covariate_dim)
+        E_u_zp1T_new = np.zeros((T, 1+D, K))
+        E_u_zp1T_new[:, 0, :] = E_u_zp1T[:, :K, :].sum(axis=1)
+        E_u_zp1T_new[:, 1:, :] = E_u_zp1T[:, K:, :]
+        E_u_zp1T = E_u_zp1T_new
+
+        E_uuT_new = np.zeros((T, 1+D, 1+D))
+        E_uuT_new[:,0,0] = E_uuT[:, :K, :K].sum(axis=(1,2))
+        E_uuT_new[:,0,1:] = E_uuT[:, :K, K:].sum(axis=1)
+        E_uuT_new[:,1:,0] = E_uuT[:, K:, :K].sum(axis=2)
+        E_uuT_new[:,1:,1:] = E_uuT[:, K:, K:]
+        E_uuT = E_uuT_new
+
+        E_u_new = np.zeros((T, 1+D))
+        E_u_new[:,0] = E_u[:, :K].sum(axis=1)
+        E_u_new[:,1:] = E_u[:, K:]
+        E_u = E_u_new
+
+        # Update statistics each row of A
+        for k in range(self.D_out):
+            Jk = self.J_0 + 2 * np.einsum('t, tij -> ij', lambda_bs[:, k], E_uuT)
+            hk = self.h_0 + E_u_zp1T[:, :, k].sum(0)
+            hk -= np.einsum('t, ti -> i', (0.5 - 2 * lambda_bs[:, k] * a), E_u)
+
+            ak = np.linalg.solve(Jk, hk)
+            self.logpi[:, k] = ak[0]
+            self.W[:, k] = ak[1:]
 
     ### Mean field
     @property